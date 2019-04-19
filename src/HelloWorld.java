96305040b611c9c310c2805d2c0d0635f91bd043
g17964ya <g17964ya@gm.tsuda.ac.jp>
2019-04-19 17:16:06 +0900

    文字列の追加！！！！！！

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,11 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!!");
 		System.out.println("Hello, Japan!!");
+		System.out.println("Hello, Canada!!");
+		System.out.println("Hello, USA!!");
+		for(int i=0;i<10;i++){
+			System.out.println("つかれた");
+		}
 	}
 
 }
