1d10701b4ec4f9231d0b14d2f1fd55387f532d78
g17964ya <g17964ya@gm.tsuda.ac.jp>
2019-04-19 17:36:55 +0900

    もっとつかれた

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 		System.out.println("Hello, Japan!!");
 		System.out.println("Hello, Canada!!");
 		System.out.println("Hello, USA!!");
-		for(int i=0;i<10;i++){
+		for(int i=0;i<20;i++){
 			System.out.println("つかれた!!!");
 		}
 	}
